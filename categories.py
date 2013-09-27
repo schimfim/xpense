a0c1ab38264436870e8ffec82ae2fe49d8a906e5
uncleWalter <schimfim@mac.com>
2013-09-27 10:41:53 -0700

    Auto commit by giteasy

---
+++ categories.py
@@ -0,0 +1,96 @@
+{
+  "Fix": {
+    "Monat": {
+      "Auto": [
+        "LASTSCHRIFT ING-DIBA"
+      ], 
+      "Haus": [
+        "WASSERABSCHLAG", 
+        "HAUPTDARLEHENSNR", 
+        "VATTENFALL", 
+        "E.ONHANSE"
+      ], 
+      "Versicherungen": [
+        "SIGNAL KRANKENVERS", 
+        "HDI LEBENSVERSICHERUNG"
+      ], 
+      "Telekom": [
+        "TELEKOM"
+      ], 
+      "Kinder": [
+        "FAM.ANTEIL KITA", 
+        "MUSIKALISCHE FRUEHERZIEHUNG", 
+        "BETREUUNGSENTGELT", 
+        "RWsoft"
+      ], 
+      "Einkommen": [
+        "VERDIENSTABRECHNUNG", 
+        "BARBARA DOERING"
+      ]
+    }, 
+    "Quartal": {
+      "Steuer": [
+        "GRUNDST"
+      ], 
+      "Diverse": [
+        "BLAU-WEISS-ROT"
+      ]
+    }
+  }, 
+  "Sonder": {
+    "Sonder": {
+      "Einmal": [
+        "REINE,DR. AUSZAHLUNG REINE"
+      ]
+    }, 
+    "Erstattet": {
+      "Arzt": [
+        "LOGOPAEDIE", 
+        "MEDISERV"
+      ]
+    }
+  }, 
+  "Variabel": {
+    "Lebenshaltung": {
+      "Supermarkt": [
+        "EDEKA", 
+        "REWE", 
+        "ERDKORN", 
+        "BUDNI", 
+        "METRO", 
+        "M.SIMON", 
+        "GUT WULKSFELDE", 
+        "FAMILA", 
+        "IHR REAL", 
+        "GETRAENKE", 
+        "SKY MARKT", 
+        "NETTO"
+      ], 
+      "Benzin": [
+        "SHELL"
+      ]
+    }, 
+    "Bar": {
+      "GA-City": [
+        "HH-SPEI"
+      ], 
+      "Kreditarte": [
+        "KREDITKARTE"
+      ], 
+      "GA-Vdorf": [
+        "HH-VOLK"
+      ]
+    }, 
+    "Konsum": {
+      "Diverse": [
+        "PFLKOELLE", 
+        "HAGEL HAIRCOMPANY AEZ"
+      ]
+    }, 
+    "Freizeit": {
+      "Restaurant": [
+        "BLOCK HOUSE"
+      ]
+    }
+  }
+}
