c0a61e7ae40b7ffc94ea86fbab1569e22b97f5b6
uncleWalter <schimfim@mac.com>
2013-09-27 10:41:49 -0700

    Auto commit by giteasy

---
+++ importer.py
@@ -0,0 +1,56 @@
+# Importer
+
+import csv
+import hashlib
+import string
+import glob
+import shelve
+import logging
+logging.basicConfig(level=logging.INFO)
+from dboximp import import_dropbox
+
+dbname = 'xpense.db'
+
+def str2f(s):
+	# remove 1000 sep
+	s1 = s.replace('.', '')
+	# change dec sep
+	s2 = s1.replace(',', '.')
+	return s2
+
+def import_file(lines):
+	imported = 0
+	skipped = 0
+	# Import one csv file
+	reader = csv.DictReader(lines, delimiter = ';')
+	db = shelve.open(dbname)
+	for row in reader:
+		# gather data
+		amt = str2f(row['Soll']) + str2f(row['Haben'])
+		(date, note) = row['Buchungstag'], row['Verwendungszweck']
+		(dy,mo,yr) = string.split(date, '.')
+		key = hashlib.md5(date + amt + note).hexdigest()
+		d = dict(year=yr, orig_year=yr, month=mo, orig_month=mo, orig_day=dy, day=dy, amount=float(amt), note=note, type='', subtype='', category='')
+		# write to db
+		if db.has_key(key):
+			logging.warning('Skipping duplicate %s', note[0:8])
+			skipped += 1
+		else: 
+			db[key] = d
+			imported += 1
+			
+	logging.info('Imported: %i', imported)
+	logging.info('Skipped: %i', skipped)
+	db.close()
+	
+def import_all():
+	files = import_dropbox(True)
+	for f in files:
+		import_file(f)
+
+def load():
+	db = shelve.open(dbname, writeback=True)
+	return db
+
+if __name__ == '__main__':
+	import_all()
